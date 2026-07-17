048228fb617bdc48b4d86af8572ce67fa2f6c74f
mo_shark <medoelzarkalt@gmail.com>
2026-07-17 19:16:38 -0400

    stopped removing supplementaries candy

--- kubejs/startup_scripts/supplementaries/removals.js
+++ kubejs/startup_scripts/supplementaries/removals.js
@@ -1,11 +1,11 @@
 function removals_Supplementaries() {
     global.REMOVALS.add([
-        'supplementaries:candy',
+        // 'supplementaries:candy',
         'supplementaries:sconce',
         'supplementaries:sconce_soul',
     ])
 
-    global.ITEM_SWAPPER.set('supplementaries:candy', 'cobblemon:rare_candy')
+    // global.ITEM_SWAPPER.set('supplementaries:candy', 'cobblemon:rare_candy')
 
     global.BLOCK_SWAPPER.set('supplementaries:sconce', 'nomansland:sconce_torch')
     global.BLOCK_SWAPPER.set('supplementaries:sconce_soul', 'nomansland:sconce_soul_torch')
